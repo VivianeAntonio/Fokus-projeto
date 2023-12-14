c3a30e95a4dfc59243872addaa8f4af4bf76ebab
VivianeAntonio <asilvaviviane@gmail.com>
2023-12-14 18:45:10 -0300

    Refatorando mudança de contexto do  evento de click

--- script.js
+++ script.js
@@ -4,18 +4,19 @@ const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 
-focoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco')
-    banner.setAttribute('src','/imagens/foco.png')
+focoBt.addEventListener('click', () => {   
+    alterarContexto('foco')
 })
 
-curtoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto')
-    banner.setAttribute('src','/imagens/descanso-curto.png')
+curtoBt.addEventListener('click', () => {   
+    alterarContexto('descanso-curto')
 })
 
-longoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo')
-    banner.setAttribute('src','/imagens/descanso-longo.png')
+longoBt.addEventListener('click', () => {   
+    alterarContexto('descanso-longo')
 })
 
+function alterarContexto(contexto) {
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `/imagens/${contexto}.png`)
+}
