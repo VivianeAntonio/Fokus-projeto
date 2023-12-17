a95f79a1da2ddd138f7cd8a91a2b41745109a592
VivianeAntonio <asilvaviviane@gmail.com>
2023-12-17 07:32:30 -0300

    Manipulando classes dos botões para ativo e não ativo

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-const html = document.querySelector('html');
-const focoBt = document.querySelector('.app__card-button--foco');
-const curtoBt = document.querySelector('.app__card-button--curto');
-const longoBt = document.querySelector('.app__card-button--longo');
-const banner = document.querySelector('.app__image');
-const titulo = document.querySelector('.app__title');
+const html = document.querySelector('html')
+const focoBt = document.querySelector('.app__card-button--foco')
+const curtoBt = document.querySelector('.app__card-button--curto')
+const longoBt = document.querySelector('.app__card-button--longo')
+const banner = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
+const botoes = document.querySelectorAll('.app__card-button')
 
 focoBt.addEventListener('click', () => {   
     alterarContexto('foco')
+    focoBt.classList.add('active')
 })
 
 curtoBt.addEventListener('click', () => {   
     alterarContexto('descanso-curto')
+    curtoBt.classList.add('active')
 })
 
 longoBt.addEventListener('click', () => {   
     alterarContexto('descanso-longo')
+    longoBt.classList.add('active')
 })
 
-function alterarContexto(contexto) {
+function alterarContexto(contexto) {    
+    botoes.forEach( function (contexto) {
+        contexto.classList.remove('active')
+    })
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `/imagens/${contexto}.png`)
     switch (contexto) {
