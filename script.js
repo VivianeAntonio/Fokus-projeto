335b2e982b6ec059c8ce93a11c2772fc0639ca33
VivianeAntonio <asilvaviviane@gmail.com>
2023-12-17 08:17:58 -0300

    Criando temporizador

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ const longoBt = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
 const titulo = document.querySelector('.app__title')
 const botoes = document.querySelectorAll('.app__card-button')
+const startPauseBt = document.querySelector('#start-pause')
 const musicaFocoInput = document.querySelector('#alternar-musica')
 const musica = new Audio('/sons/luna-rise-part-one.mp3')
+
+let tempoDecorridoEmSegundos = 5
+let intervaloId = null
+
 musica.loop = true
 
 musicaFocoInput.addEventListener('change', () => {
@@ -56,11 +61,20 @@ function alterarContexto(contexto) {
             Hora de voltar à superfície.<br>
                 <strong class="app__title-strong">Faça uma pausa longa!</strong>
             `
-            break;
-    
-    
-    
+            break;      
         default:
             break;
     }
+}
+
+const contagemRegressiva = () => {    
+    //iniciar()
+    tempoDecorridoEmSegundos -= 1
+    console.log('temporizador: ' + tempoDecorridoEmSegundos)
+}
+
+startPauseBt.addEventListener('click', contagemRegressiva)
+
+function iniciar() {    
+    intervaloId = setInterval(contagemRegressiva, 1000)
 }
